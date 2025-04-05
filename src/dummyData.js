67a90710faa60bea7a7f5d2a5da9ca8a00845c44
Wix-Code <ogbonna428alex@gmail.com>
2025-04-05 13:26:36 +0100

    push

--- src/dummyData.js
+++ src/dummyData.js
@@ -24,6 +24,14 @@ export const Links = [
 export const myProjects = [
   {
     id: 1,
+    title: 'Nairaxi',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius, sem non fermentum dictum, justo est sollicitudin lectus, at consectetur ligula neque vitae ipsum.',
+    stack: "Postgres, Express, React Js, Node Js, Prisma ",
+    image: './pic/pic3.png',
+    link: 'https://u-ride-rust.vercel.app/',
+  },
+  {
+    id: 2,
     title: 'Tasty',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius, sem non fermentum dictum, justo est sollicitudin lectus, at consectetur ligula neque vitae ipsum.',
     stack: "TypeScript, Mongo DB, Express Js, Node Js",
@@ -31,7 +39,7 @@ export const myProjects = [
     link: 'https://food-orderng.vercel.app/',
   },
   {
-    id: 2,
+    id: 3,
     title: 'Realtor',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius, sem non fermentum dictum, justo est sollicitudin lectus, at consectetur ligula neque vitae ipsum.',
     stack: "Mongo DB, Express Js, React Js, Node Js (MERN)",
@@ -39,11 +47,27 @@ export const myProjects = [
     link: 'https://realtor-frontend-xi.vercel.app/',
   },
   {
-    id: 2,
-    title: 'Realtor',
+    id: 4,
+    title: 'W.I.S',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius, sem non fermentum dictum, justo est sollicitudin lectus, at consectetur ligula neque vitae ipsum.',
-    stack: "Mongo DB, Express Js, React Js, Node Js (MERN)",
+    stack: "React Js",
+    image: './pic/pic2.png',
+    link: 'https://wix-edu-ng.vercel.app/',
+  },
+  {
+    id: 5,
+    title: 'Real Estate',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius, sem non fermentum dictum, justo est sollicitudin lectus, at consectetur ligula neque vitae ipsum.',
+    stack: "React Js",
+    image: './pic/pic4.png',
+    link: 'https://real-estate-site-eta-seven.vercel.app/',
+  },
+  {
+    id: 6,
+    title: 'W.I.S',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius, sem non fermentum dictum, justo est sollicitudin lectus, at consectetur ligula neque vitae ipsum.',
+    stack: "React Js",
     image: './pic/pic.png',
-    link: 'https://realtor-frontend-xi.vercel.app/',
-  }
+    link: 'https://wix-edu-ng.vercel.app/',
+  },
 ]
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
           <h2 data-aos="fade-up">I'm always active to turn your ideas into reality</h2>
           <h3 data-aos="fade-up">If you can think it, <span>I will build it!</span></h3>
           <p data-aos="fade-up">I’m always open to new opportunities and collaborations. Contact me via email, phone, or social media. I look forward to hearing from you soon.</p>
-          <div data-aos="up" className='href'>
+          <div data-aos="fade-up" className='href'>
             <a href="mailto:ogbonna428alex@gmail.com">Send mail <IoIosMail className='ref' /></a>
             <a href="tel:+2348126829146">Call me <FiPhoneCall className='ref' /></a>
             <a href="https://wa.me/08126829146">WhatsApp <FaWhatsapp className='ref' /></a>
