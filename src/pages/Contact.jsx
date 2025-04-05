f6d99931e1241a71d3d4307be91e7eea8ad3a78f
Wix-Code <ogbonna428alex@gmail.com>
2025-04-05 02:55:03 +0100

    push

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./contact.css"
+import { FaFacebook, FaGithub, FaLinkedin, FaTwitter, FaWhatsapp } from 'react-icons/fa6'
+import { IoIosMail } from 'react-icons/io'
+import { FiPhoneCall } from 'react-icons/fi'
 
 const Contact = () => {
   return (
@@ -8,7 +11,19 @@ const Contact = () => {
       <div className='contact'>
         <div className='cont'>
           <h2>I'm always active to turn your ideas into reality</h2>
-          <h3>If you can think it, We will build it!</h3>
+          <h3>If you can think it, <span>I will build it!</span></h3>
+          <p>I’m always open to new opportunities and collaborations. Contact me via email, phone, or social media. I look forward to hearing from you soon.</p>
+          <div className='href'>
+            <a href="mailto:ogbonna428alex@gmail.com">Send mail <IoIosMail className='ref' /></a>
+            <a href="tel:+2348126829146">Call me <FiPhoneCall className='ref' /></a>
+            <a href="https://wa.me/08126829146">WhatsApp <FaWhatsapp className='ref' /></a>
+          </div>
+          <div data-aos="fade-up" className='icon'>
+            <a href="https://gibhub.com/Wix-Code"><FaGithub /></a>
+            <a href="https://web.facebook.com/profile.php?id=61573972061573"><FaFacebook /></a>
+            <a href="http://"><FaTwitter /></a>
+            <a href="https://www.linkedin.com/in/ogbonna-wisdom-23958420b/"><FaLinkedin /></a>
+          </div>
         </div>
         <div className='contact_img'>
           <img src="https://www.shutterstock.com/image-photo/using-laptop-show-icon-address-600nw-2521386695.jpg" alt="" />
